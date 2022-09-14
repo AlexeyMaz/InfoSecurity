7d9a68c4d528f1e5812be1ce56a9e386bef0517f
Al Maz <alexmaznitskiy@gmail.com>
2022-09-14 23:11:04 +0300

    LR2_2 done

---
+++ LR2_2/LR2_2/LR2_2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str; int sum = 0;
+    ifstream input("input.txt");
+    ofstream output("output.txt");
+    while (!input.eof())
+    {
+        getline(input, str);
+        for (int i = 0; i < size(str); ++i)
+            sum += str[i] - 48;
+        output << 9 * (sum / 9 + 1) - sum;
+    }
+    input.close();
+    output.close();
+}
