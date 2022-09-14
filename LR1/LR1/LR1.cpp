9ae2443965035a21f7e329abaebd41af22a1cab1
Al Maz <alexmaznitskiy@gmail.com>
2022-09-14 09:03:26 +0300

    LR1 done

---
+++ LR1/LR1/LR1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	string s, word, t; int i;
+	vector <string> all_words;
+	ifstream input("input.txt");
+	ofstream output("output.txt");
+
+	while (!input.eof())
+	{
+		getline(input, s);
+		stringstream str(s);
+		while (str >> word)
+		{
+			all_words.push_back(word);
+		}
+		for (i = 0; i < all_words.size(); i++)
+		{
+			t = all_words[i];
+			if (size(all_words[i]) > 3)
+				rotate(t.rbegin(), t.rbegin() + 3, t.rend());
+			else if (size(all_words[i]) == 2)
+				rotate(t.rbegin(), t.rbegin() + 1, t.rend());
+			output << t + ' ';
+			cout << t + ' ';
+		}
+	}
+	input.close();
+	output.close();
+	cout << "\n";
+}
