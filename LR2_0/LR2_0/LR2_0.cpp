15e71900097d89cc9836b3152a32adedf0cc4b9d
Al Maz <alexmaznitskiy@gmail.com>
2022-09-14 22:10:54 +0300

    LR2_0 done

---
+++ LR2_0/LR2_0/LR2_0.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str; int kol; char tmp;
+    ifstream input("input.txt");
+    ofstream output("output.txt");
+    while (!input.eof())
+    {
+        getline(input, str);
+        char tmp = str[0]; kol = 1;
+        for (int i = 1; i < size(str); ++i)
+        {
+            if (tmp != str[i])
+            {
+                if (kol > 1) output << to_string(kol);
+                output << str[i - 1];
+                tmp = str[i]; kol = 0;
+            }
+            if (tmp == str[i])
+            {
+                kol++;
+            }
+        }
+        if (kol > 1) output << to_string(kol);
+        output << str[size(str) - 1];
+    }
+    input.close();
+    output.close();
+}
