996f5f932494d5b59a2c04719f22d69765433a1a
Al Maz <alexmaznitskiy@gmail.com>
2022-09-25 19:24:13 +0300

    LR3_0 done

---
+++ LR3_0/LR3_0/LR3_0.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+int main() {
+	ifstream input("input.txt");
+	string text;
+	input >> text;
+	input.close();
+
+	int i = 0, len = text.length();
+	while (i < len - 1)
+	{
+		if (text[i] == text[i + 1])
+		{
+			text = text.substr(0, i) + text.substr(i + 2, len - i - 2);
+			if (i > 0)
+				i--;
+			len -= 2;
+		}
+		else
+			i++;
+	}
+
+	ofstream output("output.txt");
+	output << text;
+}
