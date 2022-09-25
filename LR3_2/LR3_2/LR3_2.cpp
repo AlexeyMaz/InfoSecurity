38f4cc526c5665dea6c65a7ad90fbea662f01cc7
Al Maz <alexmaznitskiy@gmail.com>
2022-09-25 21:26:58 +0300

    LR3_2 done

---
+++ LR3_2/LR3_2/LR3_2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void main() {
+	ifstream input("input.txt");
+	string res = "", str = "";
+	vector<int> inds;
+	int count = 0, last_ind = 0;
+	input >> str; input.close();
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		for (int j = 1; j < str.length(); j++)
+			if (str[i] == str[j])
+			{
+				count++;
+				last_ind = j;
+			}
+
+		if (count == 0)
+		{
+			if (find(inds.begin(), inds.end(), last_ind) == inds.end())
+				inds.push_back(i);
+		}
+		else
+		{
+			count = 0;
+			if (find(inds.begin(), inds.end(), last_ind) == inds.end())
+				inds.push_back(last_ind);
+		}
+	}
+
+	sort(inds.begin(), inds.end());
+	for (int i = 0; i < inds.size(); i++)
+		res += str[inds[i]];
+
+	ofstream output("output.txt");
+	output << res;
+}
