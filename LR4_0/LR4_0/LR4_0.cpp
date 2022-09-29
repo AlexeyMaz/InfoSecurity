62544ec8faee90082d5392444ff06b6c30213684
unknown <94486568+AlexeyMaz@users.noreply.github.com>
2022-09-29 16:31:03 +0300

    LR4_0 & unfinished LR4_2

---
+++ LR4_0/LR4_0/LR4_0.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+using namespace std;
+
+int n;
+int k;
+vector<char> CompObj;
+vector<char> alphabet;
+
+string result = "";
+
+void InputAlpha()
+{
+	n = 3;
+	k = 8;
+	alphabet.resize(n);
+	alphabet = { '2','5','7' };
+}
+
+bool Condition(string inp)
+{
+	if (count(inp.begin(), inp.end(), '2') == 2 && count(inp.begin(), inp.end(), '5') == 2)
+		return true;
+	else
+		return false;
+}
+
+void PrintCompObjWithCondition(int k)
+{
+	string ObjToPrint = "";
+	for (int i = 0; i < k; i++)
+	{
+		ObjToPrint += CompObj[i];
+	}
+	if (Condition(ObjToPrint))
+		result += ObjToPrint + "\n";
+}
+
+bool HasNextCompObj()
+{
+	int kol = 0;
+	for (int i = 0; i < k; i++)
+		if (CompObj[i] == alphabet[n - 1]) kol++;
+	return (kol != k);
+}
+void NextCompObj()
+{
+	for (int i = k - 1; i > -1; i--)
+	{
+		if (CompObj[i] == alphabet[n - 1])
+		{
+			CompObj[i] = alphabet[0];
+			continue;
+		}
+		CompObj[i]++;
+
+		while (!(find(alphabet.begin(), alphabet.end(), CompObj[i]) != alphabet.end()))
+		{
+			CompObj[i]++;
+		}
+		break;
+	}
+}
+
+void Process()
+{
+	CompObj.resize(k);
+	for (int i = 0; i < k; i++)
+		CompObj[i] = alphabet[0];
+	while (HasNextCompObj())
+	{
+		PrintCompObjWithCondition(k);
+		NextCompObj();
+	}
+	PrintCompObjWithCondition(k);
+}
+
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+	InputAlpha();
+	Process();
+	fstream Result;
+	Result.open("output.txt", fstream::in | fstream::out | fstream::app);
+	Result << result << endl;
+	Result.close();
+}
---
+++ LR4_2/LR4_2/LR4_2.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ifstream input("alarm.in.txt");
+    vector<int> d;
+    vector<vector<int>> corridors;
+    int res = 0, n = 0;
+    string s;
+    
+    getline(input, s);
+    for (int i = 0; i < s.size(); ++i) 
+    {
+        n += s[i] - 48;
+        n *= 10;
+    }
+    n /= 10;
+
+    getline(input, s);
+    for (int i = 0; i < n * 2 - 1; ++i)
+        if (s[i] != ' ') 
+            d.push_back(s[i] - 48);
+
+    vector<int> tmp(3);
+    while (!input.eof())
+    {
+        copy_n(istream_iterator<int>(input), 3, tmp.begin());
+        corridors.push_back(tmp);
+    }
+    
+    for (int i = 0; i < corridors.size(); ++i) {
+        for (int j = 0; j < 3; ++j)
+            cout << corridors[i][j] << " ";
+        cout << endl;
+    }
+
+    ofstream output("alarm.out.txt");
+    output << res;
+}
